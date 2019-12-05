62d488c641d54f6e8df04a0a897f232012584784
zaru <sakuraba@basicinc.jp>
2019-12-05 16:43:59 +0900

    remove file

--- write-cookie.php
+++
@@ -1,6 +0,0 @@
-<?php
-$value = rand();
-setcookie('php-cookie', $value, time()+60*60*24*30, '/', '06dc575a.ngrok.io');
-setcookie('php-cookie', $value, time()+60*60*24*30, '/', '06dc575a.ngrok.io', true, true);
-?>
-set cookie
